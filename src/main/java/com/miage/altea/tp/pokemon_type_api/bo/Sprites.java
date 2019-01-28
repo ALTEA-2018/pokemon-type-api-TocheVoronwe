9d9aeca5f58d72183cd7348dc206f06e64c039a0
toche <christophe.maingard@epitech.eu>
2019-01-28 10:23:18 +0100

    feat(pokemon): init project

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/bo/Sprites.java
@@ -0,0 +1,11 @@
+package com.miage.altea.tp.pokemon_type_api.bo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Sprites {
+    private String back_default;
+    private String front_default;
+}
---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonTypeRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.miage.altea.tp.pokemon_type_api.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class PokemonTypeRepositoryImpl implements PokemonTypeRepository {
+
+    private List<PokemonType> pokemons;
+
+    public PokemonTypeRepositoryImpl() {
+        try {
+            var pokemonsStream = this.getClass().getResourceAsStream("/pokemons.json"); 
+
+            var objectMapper = new ObjectMapper();
+            var pokemonsArray = objectMapper.readValue(pokemonsStream, PokemonType[].class);
+            this.pokemons = Arrays.asList(pokemonsArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public PokemonType findPokemonTypeById(int id) {
+        System.out.println("Loading Pokemon information for Pokemon id " + id);
+        if (id < 1)
+            return null;
+        return pokemons.stream().filter(pokemonType -> pokemonType.getId() == id).findFirst().orElseGet(null);
+    }
+
+    @Override
+    public PokemonType findPokemonTypeByName(String name) {
+        System.out.println("Loading Pokemon information for Pokemon name " + name);
+        if (name.isEmpty())
+            return null;
+        return pokemons.stream().filter(pokemonType -> pokemonType.getName().equals(name)).findFirst().orElseGet(null);
+    }
+
+    @Override
+    public List<PokemonType> findAllPokemonType() {
+        return pokemons;
+    }
+}
